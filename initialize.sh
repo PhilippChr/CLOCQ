98a8719dacc370fdee5795a5e91c9d0d5905140c
Philipp Christmann <pchristm@mpi-inf.mpg.de>
2022-02-08 10:27:02 +0100

    Small fix for initializing script

--- initialize.sh
+++ initialize.sh
@@ -4,12 +4,13 @@ mkdir data
 mkdir results
 wget http://qa.mpi-inf.mpg.de/clocq/clocq_data.zip
 unzip clocq_data.zip -d data
+rm clocq_data.zip
 
 # download wiki2vec embeddings
 echo "Downloading wikipedia2Vec embeddings..."
 wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_300d.pkl.bz2 
-bunzip2 -d enwiki_20180420_300d.pkl.bz2 data/
-rm enwiki_20180420_300d.pkl.bz2
+bunzip2 -d enwiki_20180420_300d.pkl.bz2 
+mv enwiki_20180420_300d.pkl data/
 
 # initialize folders
 mkdir results/
